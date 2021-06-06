7cc9e13291ccc2d16f26868508297948fb78a7ab
user <user@Kitchen>
2021-06-05 17:27:44 -0700

    Venn Diagram and Unbirthday

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,15 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String input=JOptionPane.showInputDialog("When is your birthday?");
+					if(input.equals("08/17")) {
+						JOptionPane.showMessageDialog(null, "Happy Birthday!");
+					}
+	}
+
+}
